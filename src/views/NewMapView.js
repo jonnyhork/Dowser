9a233ae7ee16ad305b8c474579aadab69d3985d7
Jamie Tanzar <jtanzar@gmail.com>
2017-11-20 00:40:30 -0700

    tweaked marker styles for map

--- src/views/NewMapView.js
+++ src/views/NewMapView.js
@@ -48,17 +48,6 @@ export default class NewMapView extends Component {
 }
 
 const styles = StyleSheet.create({
-  radius: {
-    height: 50,
-    width: 50,
-    borderRadius: 50/2,
-    overflow: 'hidden',
-    borderColor: 'rgba(0, 122, 255, 0.1)',
-    borderWidth: 1,
-    borderColor: 'rgba(0, 122, 255, 0.3)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   marker: {
     height: 20,
     width: 20,
