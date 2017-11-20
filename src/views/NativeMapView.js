28f592c197e09328ad0b4118c925d7026ce17b1e
Jonny Hork <JonnyHork@gmail.com>
2017-11-20 13:55:55 -0700

    Map markers will vary in sioze by total number of checkins.

--- src/views/NativeMapView.js
+++ src/views/NativeMapView.js
@@ -9,7 +9,7 @@ export default class NativeMapView extends Component {
 
 
   render() {
-    // console.log('searchResults in map', this.props.searchResults)
+    console.log('searchResults in map', this.props.searchResults)
     // console.log("User location mapview props", this.props.userLocation)
     return (
 
@@ -27,13 +27,22 @@ export default class NativeMapView extends Component {
             {
               this.props.searchResults.map( i => (
               <MapView.Marker
+                key={i.location[0]}
                 coordinate={{
                   latitude: i.location[0],
                   longitude: i.location[1],
                 }}
                 title={ i.name }>
 
-                <View style={styles.marker} />
+                <View style={{
+                  height: (i.checkinCount/800),
+                  width: (i.checkinCount/800),
+                  borderWidth: 1,
+                  borderColor: 'blue',
+                  borderRadius: ((i.checkinCount/800)/2),
+                  overflow: 'hidden',
+                  backgroundColor: 'rgba(0, 217, 255, 0.38)'
+                }} />
 
               </MapView.Marker>
             ))
