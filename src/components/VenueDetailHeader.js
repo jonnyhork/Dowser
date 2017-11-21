6df24c1379f1b178bffefbc9db542f7836c8f86c
Jonny Hork <JonnyHork@gmail.com>
2017-11-21 07:22:22 -0700

    Add a photo to the venue detail header.

--- src/components/VenueDetailHeader.js
+++ src/components/VenueDetailHeader.js
@@ -8,8 +8,18 @@ class VenueDetailHeader extends Component {
   render () {
     return (
       <View>
+        <View>
+          <Text>Blue Bottle Coffee</Text>
+        </View>
+        <Image
+          style={{width: 150, height: 150}}
+          source={{uri: 'https://igx.4sqi.net/img/general/original/364676_tgNSuxJ7uTY1cfDpVWEGREiJxFdEruUK70nLjXL3OUs.jpg'}}
+          />
 
-         <Text>This is the detial header</Text>
+      <View>
+          <Text>This is the detial header</Text>
+
+        </View>
 
       </View>
     )
