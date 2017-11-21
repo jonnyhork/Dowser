a8049dd62f0a8719ecdc6e5aa2dbdd46bdcb4f61
Jamie Tanzar <jtanzar@gmail.com>
2017-11-21 13:25:57 -0700

    activity indicator will be solid color matching xcode landing

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -21,16 +21,7 @@ class LoginView extends Component {
             backgroundColor='rgba(117,206,182,0.8)'
             borderRadius={15}
             onPress={() => this.props.loginWithGoogle()}
-            />
-            {/* <Button
-              large
-              icon={{name: 'rocket', type: 'font-awesome'}}
-              title='VenueDetailView'
-              backgroundColor='rgba(15, 124, 237, 0.64)'
-              borderRadius={20}
-              onPress={() => Actions.VenueDetailView()}
-              /> */}
-
+          />
         </View>
       </View>
     )
