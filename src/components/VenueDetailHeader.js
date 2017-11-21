007c371884d6aec32e9f7621a550970a97e013a1
Jonny Hork <JonnyHork@gmail.com>
2017-11-20 18:49:31 -0700

    Creates a VenueDetailView componoent.

--- src/components/VenueDetailHeader.js
+++ src/components/VenueDetailHeader.js
@@ -2,3 +2,12 @@ import React, { Component } from 'react'
 import {ActivityIndicator, View, Text, Image} from 'react-native'
 import style from '../styles'
 import { Avatar } from 'react-native-elements'
+
+class VenueDetailHeader extends Component {
+
+  
+
+}
+
+
+export defualt VenueDetailHeader
