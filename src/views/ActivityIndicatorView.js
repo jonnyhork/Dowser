85ad6610d796439acd17486a09e8ed7389f72744
Jonny Hork <JonnyHork@gmail.com>
2017-11-21 11:41:35 -0700

    nav control changes

---
+++ src/views/ActivityIndicatorView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ActivityIndicator, Text, View } from 'react-native'
+import style from '../styles'
+
+const ActivityIndicatorView = () => {
+
+  return (
+      <View>
+        <ActivityIndicator />
+      </View>
+    )
+}
+
+export default ActivityIndicatorView
